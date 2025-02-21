5ae4169edd8c2d7c1809ab499e901ef1ef155c05
Aamaxi <112830001+Aamaxi@users.noreply.github.com>
2025-02-21 13:47:36 +0100

    added index.js and express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// lksjdflksdjf
+const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+
+
+app.get("/", function(request, response){
+  response.send("Hello, world!");
+
+});
