088085b30e65979c5dcaed6658db73a70b5eb3c9
Aamaxi <112830001+Aamaxi@users.noreply.github.com>
2025-02-21 14:12:57 +0100

    added .env

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+const conn = mysql.createConnection({
+  host: DB_HOST,
+  password: DB_PASSWORD,
+  user: "avnadmin",
+  database: "defaultdb",
+  port: DB_PORT, 
+  ssl: {}
+});
+
 
 app.get("/", function(request, response){
   response.send("Hello, world!");
